57ccadc1908f1a3aff75e21be396a4fcaeff7b22
giannis10 <80069359+giannis10@users.noreply.github.com>
2024-01-06 19:03:53 +0200

    Add files via upload

---
+++ Hacking_tools_installer.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+git clone --depth=1 https://github.com/htr-tech/zphisher.git
+git clone https://github.com/thehackingsage/ddos
+git clone https://github.com/giannis10/Brute-Force-SSH
+git clone https://github.com/giannis10/Brute-Force-Wifi
+git clone https://github.com/giannis10/Brute-Force-zip
+git clone https://github.com/giannis10/Brute-Force-Email
+git clone https://github.com/aydinnyunus/Keylogger
+git clone https://github.com/palahsu/DDoS-Ripper
