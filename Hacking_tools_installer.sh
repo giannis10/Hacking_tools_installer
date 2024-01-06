d25e09c6ae7301a46a60bb06e11ca7e486f611a5
giannis10 <80069359+giannis10@users.noreply.github.com>
2024-01-06 21:55:43 +0200

    Update Hacking_tools_installer.sh

--- Hacking_tools_installer.sh
+++ Hacking_tools_installer.sh
@@ -8,3 +8,5 @@ git clone https://github.com/giannis10/Brute-Force-zip
 git clone https://github.com/giannis10/Brute-Force-Email
 git clone https://github.com/aydinnyunus/Keylogger
 git clone https://github.com/palahsu/DDoS-Ripper
+git clone https://github.com/0xfff0800/Brute-force-Instagram-2023
+git clone https://github.com/IAmBlackHacker/Facebook-BruteForce
